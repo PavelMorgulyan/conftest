9a9ed96b616e09c91c04e9d4753e5276f3c76b98
PavelMorgulyan <pmorgulyan@mail.ru>
2022-11-10 13:49:27 +0300

    Change name of testing function in test_items.py

--- test_items.py
+++ test_items.py
@@ -5,7 +5,7 @@ from selenium.webdriver.common.by import By
 
 
 
-def test_guest_should_see_login_link(browser): # language
+def test_check_basket_button_exists(browser): # language
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     browser.get(link)
     time.sleep(30)
