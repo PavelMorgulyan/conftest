98cd53b1494529549e973f5c0dc9dd7ce9ab3934
PavelMorgulyan <pmorgulyan@mail.ru>
2022-11-10 13:53:37 +0300

    modified:   test_items.py

--- conftest.py
+++ conftest.py
@@ -18,7 +18,9 @@ def browser(request):
     options = Options()
     options.add_experimental_option('prefs', {'intl.accept_languages': browser_language})
     browser = webdriver.Chrome(options=options)
-    
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    browser.get(link)
+    time.sleep(30)
     yield browser
     
     print("\nquit browser..")
--- test_items.py
+++ test_items.py
@@ -6,7 +6,6 @@ from selenium.webdriver.common.by import By
 
 
 def test_check_basket_button_exists(browser): # language
-    time.sleep(30)
     button = browser.find_elements(By.CLASS_NAME, "btn.btn-lg.btn-primary.btn-add-to-basket")
     print(button)
     
